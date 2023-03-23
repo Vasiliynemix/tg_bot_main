440f24adef2f24a9919ee1bfb85f1018d3d70e4b
Vasiliy Zuev <nemixvasya@gmail.com>
2023-03-24 01:19:54 +0300

    refactor code

--- main.py
+++ main.py
@@ -21,32 +21,49 @@ async def process_start_help_command(message: Message):
 
 @dp.message()
 async def send_cat(message: Message):
+
     user_id = message.from_user.id
     print(user_id)
+
     random_count = random.randint(1, 3)
+
     if random_count == 1:
         cat_link = requests.get(API_URL_CAT).json()['file']
+
+        # начало для леры
         if user_id == lera_id:
             await bot.send_message(lera_id, "Картинка котика специально для Леры <3 наслаждайся!")
             await bot.send_sticker(chat_id=lera_id,
                                    sticker=r"CAACAgIAAxkBAAEIQW5kHKQTv3YJ1eLwiTqeBirQHAb9uAACsgAD98zUGN0m4YqZQUzILwQ")
         await bot.send_photo(chat_id=lera_id, photo=cat_link)
+        # конец для леры
+
         await bot.send_photo(chat_id=message.from_user.id, photo=cat_link)
+
     elif random_count == 2:
         dog_link = requests.get(API_URL_DOG).json()['url']
-        if user_id == lera_id or user_id == my_id:
+
+        # начало для леры
+        if user_id == lera_id:
             await bot.send_message(lera_id, "Картинка котика специально для Леры <3 наслаждайся!")
             await bot.send_sticker(chat_id=lera_id,
                                    sticker=r"CAACAgIAAxkBAAEIQW5kHKQTv3YJ1eLwiTqeBirQHAb9uAACsgAD98zUGN0m4YqZQUzILwQ")
         await bot.send_photo(chat_id=lera_id, photo=dog_link)
+        # конец для леры
+
         await bot.send_photo(chat_id=message.from_user.id, photo=dog_link)
+
     else:
         fog_link = requests.get(API_URL_FOG).json()['link']
-        if user_id == lera_id or user_id == my_id:
+
+        # начало для леры
+        if user_id == lera_id:
             await bot.send_message(lera_id, "Картинка котика специально для Леры <3 наслаждайся!")
             await bot.send_sticker(chat_id=lera_id,
                                    sticker=r"CAACAgIAAxkBAAEIQW5kHKQTv3YJ1eLwiTqeBirQHAb9uAACsgAD98zUGN0m4YqZQUzILwQ")
         await bot.send_photo(chat_id=lera_id, photo=fog_link)
+        # конец для леры
+
         await bot.send_photo(chat_id=message.from_user.id, photo=fog_link)
 
 
